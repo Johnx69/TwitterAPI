296ffe4cdd9c89e7d0b7ad1afa4efc2bd4129817
Johnx69 <hoanganh692004@gmail.com>
2023-09-11 19:50:28 -0400

    error handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import usersRouter from './routes/users.routes'
 import express from 'express'
 import databaseService from './services/database.services'
+import { NextFunction, Request, Response } from 'express-serve-static-core'
 const app = express()
 const port = process.env.PORT
 // respond with "hello world" when a GET request is made to the homepage
@@ -8,6 +9,10 @@ app.use(express.json())
 app.use('/users', usersRouter)
 
 databaseService.connect()
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(400).json({ error: err.message })
+})
 
 app.listen(port, () => {
   console.log(`Example running on ${port}`)
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,4 +1,6 @@
+import { wrapAsync } from './../utils/handlers';
 import { Router } from 'express'
+
 import { loginValidator, registerValidator } from '~/middlewares/users.middlewares'
 import { loginController, registerController } from '~/controllers/users.controllers'
 const usersRouter = Router()
@@ -11,5 +13,5 @@ usersRouter.post('/login', loginValidator, loginController)
  * Body: {name: string, email: string, password: string, confirm_password: string, date_of_birth: ISO8601}
  *
  */
-usersRouter.post('/register', registerValidator, registerController)
+usersRouter.post('/register', registerValidator, wrapAsync(registerController))
 export default usersRouter
